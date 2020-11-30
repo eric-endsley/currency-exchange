c4d62865cd4dde7355249f53ede98e24659bc4ac
Eric Endsley <eric.endsley4@gmail.com>
2020-11-29 21:57:36 -0800

    fix API url to successfully make API request

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,6 +1,6 @@
 export class ExchangeService {
   static getExRate() {
-    return fetch(`http:// https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function (response) {
         if (!response.ok) {
           throw Error(response.statusText);
