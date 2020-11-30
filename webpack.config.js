67f641a191a11c6c23c48a22c59edee1c55a408b
Eric Endsley <eric.endsley4@gmail.com>
2020-11-30 01:16:35 -0800

    add title to package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({                                                                                                                                
-      title: "",
+      title: "exchange-rate-calculator",
       template: "./src/index.html",
       inject: "body",
     }),
