d0f5f6e9d06fdc5da8bb9645137b7cd4802f967d
Eric Endsley <eric.endsley4@gmail.com>
2020-11-29 21:35:39 -0800

    write function to convert dollars using api response and output result

--- src/main.js
+++ src/main.js
@@ -4,3 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import {ExchangeService} from './exchange-service.js'
 
+function getCurrency(response) {
+  if (response.result = "success") {
+    let newCurrency = $("#currency").val();
+    let usdInput = $("#dollars").val();
+    let convertDollars = usdInput * response.`${newCurrency}`;
+    $("#output").text(convertDollars);
+  } else {
+    $("#output").text(`An error occured: ${response.message}`)
+  }
+} 
+
+
