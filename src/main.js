1adc3e903e1033b5c3e5c98d843c4a53f542fcaa
Eric Endsley <eric.endsley4@gmail.com>
2020-12-06 23:01:59 -0800

    fix bug so flag icons display, set US flag to default

--- src/main.js
+++ src/main.js
@@ -31,12 +31,18 @@ $(document).ready(function() {
         convertCurrency(response, currencyFromName);
       });
   });
-});
 
-$(document.ready(function() {
-  $(#currencyFrom).change(function() {
+  $('#currencyFrom').change(function() {
     let currencyFromArray = $('#currencyFrom').val().split(', ');
-    let currencyFromCode = currencyFromArray[2];
-    $('#flag').html(<img src="https://flagcdn.com/16x12/" + currencyFromCode ".png" srcset="https://flagcdn.com/32x24/" + currencyFromCode + ".png 2x, https://flagcdn.com/48x36/" + currencyFromCode + ".png 3x" width="16" height="12")
+    let currencyFromFlagCode = currencyFromArray[2];
+    let flagSrc = `<img src='https://flagcdn.com/16x12/${currencyFromFlagCode}.png' srcset='https://flagcdn.com/32x24/${currencyFromFlagCode}.png 2x, https://flagcdn.com/48x36/${currencyFromFlagCode}.png 3x' width='16' height='12'>`
+    $('#flag1').html(flagSrc);
+  });
+
+  $('#currency').change(function() {
+    let currencyArray = $('#currency').val().split(', ');
+    let currencyFlagCode = currencyArray[2];
+    let flagSrc2 = `<img src='https://flagcdn.com/16x12/${currencyFlagCode}.png' srcset='https://flagcdn.com/32x24/${currencyFlagCode}.png 2x, https://flagcdn.com/48x36/${currencyFlagCode}.png 3x' width='16' height='12'>`
+    $('#flag2').html(flagSrc2);
   });
 });
