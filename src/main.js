ed679b4d4c0c1eeb172002994011efa0b14492a3
Eric Endsley <eric.endsley4@gmail.com>
2020-12-06 23:20:24 -0800

    fix typo in select menu, make number input required

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ function convertCurrency(response, currencyFromName) {
 } 
 
 $(document).ready(function() {
-  $('#submit').click(function() {
+  $('#submit').submit(function() {
     let currencyFromArray = $('#currencyFrom').val().split(', ');
     let currencyFromCode = currencyFromArray[0];
     let currencyFromName = currencyFromArray[1];
@@ -30,19 +30,20 @@ $(document).ready(function() {
       .then(function(response) {
         convertCurrency(response, currencyFromName);
       });
+    return false;
   });
 
   $('#currencyFrom').change(function() {
     let currencyFromArray = $('#currencyFrom').val().split(', ');
     let currencyFromFlagCode = currencyFromArray[2];
-    let flagSrc = `<img src='https://flagcdn.com/16x12/${currencyFromFlagCode}.png' srcset='https://flagcdn.com/32x24/${currencyFromFlagCode}.png 2x, https://flagcdn.com/48x36/${currencyFromFlagCode}.png 3x' width='16' height='12'>`
+    let flagSrc = `<img src='https://flagcdn.com/16x12/${currencyFromFlagCode}.png' srcset='https://flagcdn.com/32x24/${currencyFromFlagCode}.png 2x, https://flagcdn.com/48x36/${currencyFromFlagCode}.png 3x' width='16' height='12'>`;
     $('#flag1').html(flagSrc);
   });
 
   $('#currency').change(function() {
     let currencyArray = $('#currency').val().split(', ');
     let currencyFlagCode = currencyArray[2];
-    let flagSrc2 = `<img src='https://flagcdn.com/16x12/${currencyFlagCode}.png' srcset='https://flagcdn.com/32x24/${currencyFlagCode}.png 2x, https://flagcdn.com/48x36/${currencyFlagCode}.png 3x' width='16' height='12'>`
+    let flagSrc2 = `<img src='https://flagcdn.com/16x12/${currencyFlagCode}.png' srcset='https://flagcdn.com/32x24/${currencyFlagCode}.png 2x, https://flagcdn.com/48x36/${currencyFlagCode}.png 3x' width='16' height='12'>`;
     $('#flag2').html(flagSrc2);
   });
 });
